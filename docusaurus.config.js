fcf6712947299c095814504a00081487af5fd0af
Sorata <194564851+SorataYang@users.noreply.github.com>
2025-12-22 17:55:20 +0800

    Update url and baseurl in unique by gitlab

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'http://manual.pages.brdi.com.cn',
+  url: 'http://qt-328a52.pages.brdi.com.cn:8090',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/manual/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
