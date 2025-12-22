c3a15abff8996abb41faedf1e777940f77f20174
Sorata <194564851+SorataYang@users.noreply.github.com>
2025-12-22 17:33:12 +0800

    Fix base Url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://fem.brdi.com.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/manual/manual/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
